5e3ef13614faad6ccb6f9bb9c69351e381b642b7
type9 <gabespersonal@gmail.com>
2019-10-09 12:33:17 -0700

    Added the ability for artpieces to be shown on art_index

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
+client = MongoClient()
+db = client.Type9Art
+portfolio = db.art_portfolio
+
 app = Flask(__name__, static_url_path='')
 
 @app.route('/art')
 def art_index():
-    return render_template('art_index.html')
+    return render_template('art_index.html', portfolio=portfolio.find())
+
+@app.route('/art/new')
+def add_art():
+    return render_template('art_new.html')
+@app.route('/art', methods=['POST'])
+def submit_art(): # on submission of new art
+    artpiece = {
+        'id': request.form.get('id'),
+        'title': request.form.get('title'),
+        'caption': request.form.get('caption'),
+        'description': request.form.get('description')
+    }
+    portfolio.insert_one(artpiece)
+    return redirect(url_for('art_index.html'))
 
 if __name__ == '__main__':
     app.run(debug=True)
