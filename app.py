fcd8e26e85e316373fe31a4bc69b343832722a2a
type9 <gabespersonal@gmail.com>
2019-10-09 13:59:35 -0700

    Added templates for art_view and started routing

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__, static_url_path='')
 def art_index():
     return render_template('art_index.html', portfolio=portfolio.find())
 
-@app.route('/art/new')
+@app.route('/art/new') # submitting new art
 def add_art():
     return render_template('art_new.html')
 @app.route('/art', methods=['POST'])
@@ -20,10 +20,16 @@ def submit_art(): # on submission of new art
         'id': request.form.get('id'),
         'title': request.form.get('title'),
         'caption': request.form.get('caption'),
-        'description': request.form.get('description')
+        'description': request.form.get('description'),
+        'date': request.form.get('date')
     }
     portfolio.insert_one(artpiece)
     return redirect(url_for('art_index.html'))
+
+@app.route('/art/<piece_id>') # viewing a particular piece
+def playlists_show(piece_id):
+    artpiece = portfolio.find_one({'id': piece_id})
+    return render_template('art_view.html', artpiece=artpiece)
 
 if __name__ == '__main__':
     app.run(debug=True)
