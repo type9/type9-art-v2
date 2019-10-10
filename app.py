3649350d2c886cf391dbbfb6e70f871b8e72757f
type9 <gabespersonal@gmail.com>
2019-10-10 15:46:22 -0700

    First production push. Added Procfile

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 client = MongoClient()
 db = client.Type9Art
 portfolio = db.art_portfolio
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path='')
 
 @app.route('/art')
 def art_index():
@@ -24,12 +25,33 @@ def submit_art(): # on submission of new art
         'date': request.form.get('date')
     }
     portfolio.insert_one(artpiece)
-    return redirect(url_for('art_index.html'))
+    return redirect(url_for('art_index'))
 
 @app.route('/art/<piece_id>') # viewing a particular piece
-def playlists_show(piece_id):
+def show_art(piece_id):
     artpiece = portfolio.find_one({'id': piece_id})
     return render_template('art_view.html', artpiece=artpiece)
+
+@app.route('/art/<piece_id>/edit') # editing a particular piece
+def edit_art(piece_id):
+    artpiece = portfolio.find_one({'id': piece_id})
+    return render_template('art_edit.html', artpiece=artpiece)
+@app.route('/art/<piece_id>/<piece_objectid>', methods=['POST'])
+def update_art(piece_id, piece_objectid):
+    updated_artpiece = {
+        'id': request.form.get('id'),
+        'title': request.form.get('title'),
+        'caption': request.form.get('caption'),
+        'description': request.form.get('description'),
+        'date': request.form.get('date')
+    }
+    portfolio.update_one({'_id': ObjectId(piece_objectid)}, {'$set': updated_artpiece})
+    return redirect(url_for('show_art', piece_id=piece_id))
+
+@app.route('/art/<piece_id>/remove') # editing a particular piece
+def remove_art(piece_id):
+    portfolio.delete_one({'id': piece_id})
+    return redirect(url_for('art_index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
